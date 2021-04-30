083cd3638cb71868cf6325eb6306889944c23efb
이승태 <fo_ma@naver.com>
2021-04-30 11:47:36 +0900

    API State

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import React from "react";
 import axios from "axios";
+import Movie from "./Movies";
 
 class App extends React.Component {
-    state = {
-        isLoading: true,
-        movies: [],
-    };
-    getMovies = async () => {
-        const movies = await axios.get(
-            "https://yts-proxy.now.sh/list_movies.json"
-        );
-    };
-    componentDidMount() {
-        this.getMovies();
-    }
-    render() {
-        const { isLoading } = this.state;
-        return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
-    }
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
+  };
+  componentDidMount() {
+    this.getMovies();
+  }
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <div>
+        {isLoading
+          ? "Loading..."
+          : movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+              />
+            ))}
+      </div>
+    );
+  }
 }
 
 export default App;
