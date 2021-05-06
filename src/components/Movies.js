a3379aa9ca7220aeb2131783491a0ec284ab3e33
이승태 <fo_ma@naver.com>
2021-05-06 14:26:29 +0900

    Detail and finishing

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,27 +1,38 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 import "./Movie.css";
 
-function Movie({ id, year, title, summary, poster, rating, genres }) {
+function Movie({ id, year, title, summary, poster, genres }) {
   return (
-    <li className="movie">
-      <img className="movie__poster" src={poster} alt={title} title={title} />
-      <div className="movie__data">
-        <h3 className="movie__title">{title}</h3>
-        <h4 className="movie__year">{year}</h4>
-        <ul className="movie__genres">
-          {genres.map((genre, index) => (
-            <li key={index} className="genres__genre">
-              {genre}
-            </li>
-          ))}
-        </ul>
-        <h5 className="movie__rating">
-          <span>{rating}/10.0</span>
-        </h5>
-        <p className="movie__summary">{summary.slice(0, 150)}</p>
-      </div>
-    </li>
+    <div className="movie">
+      <Link
+        to={{
+          pathname: `/movie/${id}`,
+          state: {
+            year,
+            title,
+            summary,
+            poster,
+            genres,
+          },
+        }}
+      >
+        <img src={poster} alt={title} title={title} />
+        <div className="movie__data">
+          <h3 className="movie__title">{title}</h3>
+          <h5 className="movie__year">{year}</h5>
+          <ul className="movie__genres">
+            {genres.map((genre, index) => (
+              <li key={index} className="genres__genre">
+                {genre}
+              </li>
+            ))}
+          </ul>
+          <p className="movie__summary">{summary.slice(0, 180)}...</p>
+        </div>
+      </Link>
+    </div>
   );
 }
 
